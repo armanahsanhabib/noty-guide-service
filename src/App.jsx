5faaa73fd49a57773c93f7b677444c60a49c42cc
Ahsan Habib <contact2ahsanhabib@gmail.com>
2023-09-03 22:00:16 +0600

    removed react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+// import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from '../src/pages/Home';
-import NotFound from '../src/pages/NotFound';
 import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/sohojit-task" element={<Home />} />
-        <Route element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <>
+      <Home />
+    </>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Categories from '../components/Categories';
 import Enterprise from '../components/Enterprise';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
 import Hero from '../components/Hero';
 
 const Home = () => {
     return (
         <div className='bg-[#F7FAFC]'>
+            <Header />
             <Hero />
             <Categories />
             <Enterprise />
+            <Footer />
         </div>
     );
 };
